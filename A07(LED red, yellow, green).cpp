41dcc8f60bbcffd51fd961693f3ed7676340c33d
Devil <116719491+Tejass12@users.noreply.github.com>
2024-05-08 07:27:08 +0530

    Update A07(LED red, yellow, green).cpp

--- A07(LED red, yellow, green).cpp
+++ A07(LED red, yellow, green).cpp
@@ -1,3 +1,6 @@
+/*Create a program so that when the user enters ‘b’ the green
+light blinks, ‘g’ the green light is illuminated ‘y’ the yellow
+light is illuminated and ‘r’ the red light is illuminated*/
 int red=8;
 int yellow=9;
 int green=10;
