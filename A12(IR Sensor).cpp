7275e6e53964e3e292b9796c0fc19d16e0bdf666
Devil <116719491+Tejass12@users.noreply.github.com>
2024-05-08 07:29:43 +0530

    Update A12(IR Sensor).cpp

--- A12(IR Sensor).cpp
+++ A12(IR Sensor).cpp
@@ -1,3 +1,7 @@
+/*Understanding the connectivity of Raspberry-Pi / Arduino
+with IR sensor. Write an application to detect obstacle and
+notify user using LEDs*/
+
 int ir = 13;
 int PinLed = 8;
 int val=0;
